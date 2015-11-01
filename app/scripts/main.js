0eaa031197886b86c8c97df2c255a452b99a5938
Gheorgheoiu Nicolae <nicolae.gheorgheoiu@businessmail.avelon-cetex.com>
2015-11-01 22:23:26 +0200

    Fixed Console error + added script for show/hide edit panels

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,21 @@
-
-
 'use strict';
 
 $(document).ready(function(){
   $('ul.tabs').tabs();
   $('select').material_select();
   $('.modal-trigger').leanModal();
+
+  // toggles for add/edit panels
+  $('a.add-new').on('click', function(){
+    $('.panel.list').slideToggle(300);
+    $('.panel.add').slideToggle(300);
+  });
+  $('a.edit').on('click', function(){
+    $('.panel.list').slideToggle(300);
+    $('.panel.add').slideToggle(300);
+  });
+  $('a.cancel').on('click', function() {
+    $('.panel.list').slideToggle(300);
+    $('.panel.add').slideToggle(300);
+  })
 });
